2214fd56c3848b322a086d07e7ccc82e674b59f9
Rebecca Rodgers <beccarodgers@hotmail.co.uk>
2017-03-12 16:07:30 +0000

    Add files via upload

--- Questions.java
+++ Questions.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Questions {
 
 	//public String[] toString() {
-	public static void main(String[] args) throws Exception {
+	public String[] question() throws Exception {
 
 		File fileIn = new File("SampleQuestions.txt");
 
@@ -13,7 +13,6 @@ public class Questions {
 
 			Scanner in = new Scanner(fileIn);
 			String[] allQuestions = new String[10];
-			String[][] allAnswers = new String[10][4];
 
 			while(in.hasNextLine()) {
 				String line = in.nextLine();
@@ -21,24 +20,50 @@ public class Questions {
 				String[] parts = line.split(",");
 				int questionNumber = Integer.parseInt(parts[0]);
 				String question = parts[1];
-				String[] answers = new String[4];
-				answers[0] = parts[2];
-				answers[1] = parts[3];
-				answers[2] = parts[4];
-				answers[3] = parts[5];
 
 				allQuestions[questionNumber - 1] = question;
-				allAnswers[questionNumber - 1] = answers;
 
 			}
 
-			System.out.println(Arrays.toString(allQuestions));
-			System.out.println(Arrays.deepToString(allAnswers));
+			return allQuestions;
 		}
 		catch( Exception e ) {
 			System.out.println("Problem reading file");
 			throw e;
 		}
 
+	}
+
+	public String[][] answers() throws Exception {
+
+
+		File fileIn = new File("SampleQuestions.txt");
+
+		try {
+
+			Scanner in = new Scanner(fileIn);
+			String[][] allAnswers = new String[10][4];
+
+			while(in.hasNextLine()) {
+				String line = in.nextLine();
+
+				String[] parts = line.split(",");
+				int questionNumber = Integer.parseInt(parts[0]);
+				String[] answers = new String[4];
+				answers[0] = parts[2];
+				answers[1] = parts[3];
+				answers[2] = parts[4];
+				answers[3] = parts[5];
+
+				allAnswers[questionNumber - 1] = answers;
+
+			}
+
+			return allAnswers;
+		}
+		catch( Exception e ) {
+			System.out.println("Problem reading file");
+			throw e;
+		}
 	}
 }
